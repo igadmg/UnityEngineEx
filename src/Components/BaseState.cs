a916858c2d29e8e9a7f2e30ba70780eaa1a5d0d9
Igor Karatayev <igor.cwer@gmail.com>
2018-04-13 13:47:15 +0300

    added state blending.

--- src/Components/BaseState.cs
+++ src/Components/BaseState.cs
@@ -6,11 +6,13 @@ namespace UnityEngineEx
 {
     public class BaseState : StateMachineBehaviour
     {
+        protected Animator m_Animator;
         protected BaseStateMachine m_StateMachine;
         protected AnimatorStateInfo m_StateInfo;
 
         public BaseStateMachine stateMachine { get { return m_StateMachine; } }
         public AnimatorStateInfo stateInfo { get { return m_StateInfo; } }
+        public float weight { get; protected set; }
 
         public string stateName { get { return m_StateMachine.GetStateName(m_StateInfo.shortNameHash); } }
     }
@@ -34,31 +36,33 @@ namespace UnityEngineEx
         // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
         override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
-            m_StateMachine = animator.gameObject.GetComponent<TStateMachine>();
+            m_Animator = animator;
+            m_StateMachine = m_Animator.gameObject.GetComponent<TStateMachine>();
             m_StateInfo = stateInfo;
-            m_Controller = animator.gameObject.GetComponent<TController>();
+            m_Controller = m_Animator.gameObject.GetComponent<TController>();
 
-            m_StateMachine.currentState = this;
+            m_StateMachine.StartTransition(this, m_Animator);
             if (m_StateMachine.onStateSwitch != null)
             {
                 m_StateMachine.onStateSwitch(this);
             }
 
+            weight = m_StateMachine.GetStateWeight(this, m_Animator);
             OnEnter();
         }
 
         // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
         override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
+            weight = m_StateMachine.GetStateWeight(this, m_Animator);
             OnUpdate();
         }
 
         // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
         override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
         {
+            weight = m_StateMachine.GetStateWeight(this, m_Animator);
             OnExit();
-
-            m_StateMachine.currentState = null;
         }
 
         // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
--- src/Components/BaseStateMachine.cs
+++ src/Components/BaseStateMachine.cs
@@ -10,16 +10,67 @@ namespace UnityEngineEx
     {
         protected Dictionary<int, string> m_StateNames = new Dictionary<int, string>();
         protected BaseState m_CurrentState;
+        protected BaseState m_LastTransitionState;
+        AnimatorTransitionInfo? m_CurrentTransition = null;
+        protected float m_TransitionStartTime;
 
 
 
-        public BaseState currentState { get { return m_CurrentState; } internal set { m_CurrentState = value; } }
+        public BaseState currentState { get { return m_CurrentState; } }
+        public BaseState lastState { get { return m_LastTransitionState; } }
+        public float weight
+        {
+            get {
+                if (!m_CurrentTransition.HasValue)
+                {
+                    return 1.0f;
+                }
+
+                return (Time.time - m_TransitionStartTime) / m_CurrentTransition.Value.duration;
+            }
+        }
+
+        internal void StartTransition(BaseState targetState, Animator animator)
+        {
+            m_TransitionStartTime = Time.time;
+
+            if (m_CurrentState == null)
+            {
+                m_CurrentState = targetState;
+                m_CurrentTransition = null;
+            }
+            else
+            {
+                if (animator.IsInTransition(0))
+                {
+                    m_CurrentTransition = animator.GetAnimatorTransitionInfo(0);
+                }
+                else
+                {
+                    m_CurrentTransition = null;
+                }
+
+                m_LastTransitionState = m_CurrentState;
+                m_CurrentState = targetState;
+            }
+        }
 
 
 
         public Action<BaseState> onStateSwitch = (state) => { };
 
 
+
+        internal float GetStateWeight(BaseState state, Animator animator)
+        {
+            if (!animator.IsInTransition(0))
+            {
+                m_CurrentTransition = null;
+            }
+
+            float w = weight;
+            return state == m_CurrentState ? w : state == m_LastTransitionState ? 1.0f - w : 0.0f;
+        }
 
         internal string GetStateName(int nameHash)
         {
