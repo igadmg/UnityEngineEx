7fe3d87fb5a4de95b15669e7a0e3d8db86795702
Igor Karatayev <igor.cwer@gmail.com>
2018-08-29 10:16:49 +0300

    added socket getter.

---
+++ src/SocketEx.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace UnityEngineEx
+{
+	public static class SocketEx
+	{
+		public static T GetSocket<T>(this Component c)
+			where T: Component
+		{
+			Transform t = c.transform.parent;
+			while (t != null && t.gameObject.tag == "Socket")
+			{
+				T r = t.GetComponent<T>();
+				if (r != null)
+					return r;
+
+				t = t.parent;
+			}
+
+			return null;
+		}
+	}
+}
