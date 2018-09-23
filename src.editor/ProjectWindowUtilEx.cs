45d4809d571c3e0c657462cba3088e81327d1dd8
Igor Karatayev <igor.cwer@gmail.com>
2018-09-23 23:56:34 +0300

    Spyglass added

---
+++ src.editor/ProjectWindowUtilEx.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using UnityEditor;
+
+
+
+namespace UnityEditorEx
+{
+	public static class ProjectWindowUtilEx
+	{
+		private static MethodInfo m_CreateScriptAsset;
+
+		static ProjectWindowUtilEx()
+		{
+			m_CreateScriptAsset = typeof(ProjectWindowUtil).GetMethod("CreateScriptAsset", BindingFlags.Static | BindingFlags.NonPublic);
+		}
+
+		public static void CreateScriptAsset(string templatePath, string destName)
+		{
+			m_CreateScriptAsset.Invoke(null, new object[] { templatePath, destName });
+		}
+	}
+}
