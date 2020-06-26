dcef417b2db8c9baedb8cd9b36846fbe6ac522f9
Igor Karatayev <igor.cwer@gmail.com>
2020-06-26 21:22:44 +0300

    editor holder

---
+++ src.editor/DisposableEditor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UnityEditorEx
+{
+	public class DisposableEditor<T> : IDisposable
+		where T : UnityEngine.Object
+	{
+		T item;
+		UnityEditor.Editor editor;
+
+		public UnityEditor.Editor GetEditor(T item)
+		{
+			if (item != this.item)
+			{
+				Dispose();
+				editor = UnityEditor.Editor.CreateEditor(item);
+				this.item = item;
+			}
+
+			return editor;
+		}
+
+		public void Dispose()
+		{
+			UnityEngine.Object.DestroyImmediate(editor);
+		}
+	}
+}
