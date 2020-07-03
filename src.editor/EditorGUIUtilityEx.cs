fc52d8362afd283e407c56797d50e595144a60a3
Igor Karatayev <igor.cwer@gmail.com>
2020-07-04 00:19:25 +0300

    update.

--- src.editor/EditorGUIUtilityEx.cs
+++ src.editor/EditorGUIUtilityEx.cs
@@ -1,31 +1,24 @@
 ﻿using System;
+using System.Reflection;
+using SystemEx;
 using UnityEditor;
-
-
+using UnityEngine;
 
 namespace UnityEditorEx
 {
 	public static class EditorGUIUtilityEx
 	{
+		static Lazy<MethodInfo> m_LoadIconRequired = new Lazy<MethodInfo>(() =>
+			typeof(EditorGUIUtility).GetMethod("LoadIconRequired", BindingFlags.Static | BindingFlags.NonPublic));
+
 		public static IDisposable LabelWidth(float newWidth)
 		{
-			return new EditorGUIUtilityLabelWidth(newWidth);
-		}
-	}
-
-	internal class EditorGUIUtilityLabelWidth : IDisposable
-	{
-		float oldWidth;
-
-		internal EditorGUIUtilityLabelWidth(float newWidth)
-		{
-			oldWidth = EditorGUIUtility.labelWidth;
+			float oldWidth = EditorGUIUtility.labelWidth;
 			EditorGUIUtility.labelWidth = newWidth;
-		}
 
-		public void Dispose()
-		{
-			EditorGUIUtility.labelWidth = oldWidth;
+			return DisposableLock.Lock(() => EditorGUIUtility.labelWidth = oldWidth);
 		}
+		public static Texture2D LoadIconRequired(string name)
+			=> (Texture2D)m_LoadIconRequired.Value.Invoke(null, new object[] { name });
 	}
 }
