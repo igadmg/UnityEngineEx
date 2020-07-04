df5eacef6cb99b86baebc8afd5a983e95649f375
Igor Karatayev <igor.cwer@gmail.com>
2020-07-04 13:44:38 +0300

    Added Editor Tool

---
+++ src.editor/EditorTool.cs
@@ -0,0 +1,25 @@
+﻿using SystemEx;
+using UnityEditor.EditorTools;
+using UnityEngine;
+
+namespace UnityEngineEx
+{
+	public class EditorTool<T> : EditorTool
+	{
+		GUIContent m_IconContent;
+
+		public override GUIContent toolbarIcon => m_IconContent;
+		public virtual Texture2D Image => null;
+		public virtual string Text => GetType().GetAttribute<EditorToolAttribute>()?.displayName;
+		public virtual string Tooltip => null;
+
+		protected virtual void OnEnable()
+		{
+			m_IconContent = new GUIContent() {
+				image = Image,
+				text = Text,
+				tooltip = Tooltip
+			};
+		}
+	}
+}
