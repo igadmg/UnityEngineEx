06e3b8efa64bc9ca78d40773c8035334ebce05c6
Igor Karatayev <igor.cwer@gmail.com>
2020-07-04 13:57:32 +0300

    cleanup.

--- src.editor/Templates/T4Extensions.cs
+++ src.editor/Templates/T4Extensions.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Threading;
-
+﻿
 #if NET_CORE
 
 namespace UnityEditorEx.src.editor.Templates
