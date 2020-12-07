1c7387a696db38a4e16f4e4cd67c093df88deec9
Igor Karatayev <igor.cwer@gmail.com>
2020-12-08 00:43:42 +0300

    update

---
+++ src/ProfilerEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SystemEx;
+using UnityEngine.Profiling;
+
+namespace UnityEngineEx
+{
+	public static class ProfilerEx
+	{
+#if ENABLE_PROFILER
+		public static IDisposable BeginSample(string name)
+		{
+			Profiler.BeginSample(name);
+			return DisposableLock.Lock(() => Profiler.EndSample());
+		}
+#else
+		public static IDisposable BeginSample(string name)
+			=> DisposableLock.empty;
+#endif
+	}
+}
